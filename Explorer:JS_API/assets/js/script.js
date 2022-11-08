c8c35201e312887ef25791b22243079496b564dd
Anna Gabain <anna.gabain@outlook.com>
2022-11-08 14:49:15 +0000

    Making a POST Request - Part 1 codealong

--- Explorer:JS_API/assets/js/script.js
+++ Explorer:JS_API/assets/js/script.js
@@ -3,6 +3,19 @@ const API_URL = "https://ci-jshint.herokuapp.com/api"
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"))
 
 document.getElementById("status").addEventListener("click", e => getStatus(e))
+document.getElementById("submit").addEventListener("click", e => postForm(e))
+
+async function postForm(e){
+    const form = new FormData(document.getElementById('checksform'))
+
+    const response = await fetch(API_URL, {
+                                method: "POST",
+                                headers: {
+                                        "Authorization": API_KEY,
+        },
+        body: form,
+    })
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`
@@ -22,6 +35,6 @@ async function getStatus(e) {
 
 function displayStatus(data){
     document.getElementById('resultsModalTitle').textContent=('API Key Status')
-    document.getElementById('results-content').textContent=(data.expiry)
+    document.getElementById('results-content').textContent=(`The key valid untill: ${data.expiry}`)
     resultsModal.show()
 }
